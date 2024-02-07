d9a7fa0a0a2b0088c10a922c73076dac5b992c5a
Arthur Ivanov <artimag666@gmail.com>
2024-02-07 20:41:26 +0200

    second

--- ClientAquariumApp/AddMaintenanceTypeForm.Designer.cs
+++ ClientAquariumApp/AddMaintenanceTypeForm.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            textBoxNameType = new TextBox();
+            label2 = new Label();
+            richTextBoxDescription = new RichTextBox();
+            addTypeButton = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(textBoxNameType, 0, 1);
+            tableLayoutPanel1.Controls.Add(label2, 0, 2);
+            tableLayoutPanel1.Controls.Add(richTextBoxDescription, 0, 3);
+            tableLayoutPanel1.Controls.Add(addTypeButton, 1, 7);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.Size = new Size(387, 293);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(187, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            // 
+            // textBoxNameType
+            // 
+            textBoxNameType.Dock = DockStyle.Fill;
+            textBoxNameType.Location = new Point(3, 38);
+            textBoxNameType.Name = "textBoxNameType";
+            textBoxNameType.Size = new Size(187, 23);
+            textBoxNameType.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(3, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(187, 35);
+            label2.TabIndex = 2;
+            label2.Text = "Description";
+            // 
+            // richTextBoxDescription
+            // 
+            tableLayoutPanel1.SetColumnSpan(richTextBoxDescription, 2);
+            richTextBoxDescription.Dock = DockStyle.Fill;
+            richTextBoxDescription.Location = new Point(3, 108);
+            richTextBoxDescription.Name = "richTextBoxDescription";
+            tableLayoutPanel1.SetRowSpan(richTextBoxDescription, 3);
+            richTextBoxDescription.Size = new Size(381, 99);
+            richTextBoxDescription.TabIndex = 3;
+            richTextBoxDescription.Text = "";
+            // 
+            // addTypeButton
+            // 
+            addTypeButton.Dock = DockStyle.Fill;
+            addTypeButton.Location = new Point(196, 248);
+            addTypeButton.Name = "addTypeButton";
+            addTypeButton.Size = new Size(188, 42);
+            addTypeButton.TabIndex = 4;
+            addTypeButton.Text = "Add";
+            addTypeButton.UseVisualStyleBackColor = true;
+            addTypeButton.Click += addTypeButton_Click;
+            // 
+            // AddMaintenanceTypeForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(387, 293);
+            Controls.Add(tableLayoutPanel1);
+            Name = "AddMaintenanceTypeForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "AddMaintenanceTypeForm";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private TextBox textBoxNameType;
+        private Label label2;
+        private RichTextBox richTextBoxDescription;
+        private Button addTypeButton;
     }
 }
--- ClientAquariumApp/AddMaintenanceTypeForm.cs
+++ ClientAquariumApp/AddMaintenanceTypeForm.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using ClientAquariumApp.ViewModels;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,67 @@ namespace ClientAquariumApp
 {
     public partial class AddMaintenanceTypeForm : Form
     {
+        private const string urlMaintenanceType = "http://localhost:5238/api/MaintenanceType/AddNewType";
+
         public AddMaintenanceTypeForm()
         {
             InitializeComponent();
+        }
+
+        private async void addTypeButton_Click(object sender, EventArgs e)
+        {
+            string name = textBoxNameType.Text;
+            string description = richTextBoxDescription.Text;
+
+            if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(description))
+            {
+                MessageBox.Show("Введите название типа обслуживания");
+                return;
+            }
+            var newType = new IdNameModel
+            {
+                Name = name,
+                Description = description
+            };
+
+            try
+            {
+                bool succes = await AddMeintenanceType(newType);
+                if (succes)
+                {
+                    MessageBox.Show("Новый тип обслуживания успешно добавлен!");
+                    ClearForm();
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка при добавлении нового типа обслуживания.");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}");
+
+            }
+        }
+
+        private void ClearForm()
+        {
+            textBoxNameType.Text = "";
+            richTextBoxDescription.Text = "";
+        }
+
+        private async Task<bool> AddMeintenanceType(IdNameModel newType)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlMaintenanceType);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string json = JsonConvert.SerializeObject(newType);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(urlMaintenanceType, content);
+                return response.IsSuccessStatusCode;
+            }
         }
     }
 }
